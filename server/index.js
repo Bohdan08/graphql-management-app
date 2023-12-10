4b6d75c7a6da860482b1dad99d1b5289a3d2f70e
Bohdan Martyniuk <bohdan@growthcurve.co>
2023-12-10 12:17:54 -0500

    first commit

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+require("colors");
+require("dotenv").config();
+const cors = require('cors');
+const schema = require("./schema/schema");
+const connectDB = require("./config/db");
+const port = process.env.PORT || 8000;
+
+const { graphqlHTTP } = require("express-graphql");
+
+const app = express();
+
+// Connect to database
+connectDB();
+
+app.use(cors());
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === "development",
+  })
+);
+
+app.listen(port, console.log(`Server running on port ${port}`));
